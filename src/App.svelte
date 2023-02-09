2ad0ff2b7766cd8d78e6d8c57ed2c7db4f21245a
Rainer Piil <piilrainer@gmail.com>
2023-02-09 14:27:53 +0200

    adding and sorting items

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,45 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  let message = "";
+  let items = [];
+
+  function add() {
+    if (message.trim() !== "") {
+      items.push({ name: message, isDone: false });
+      items = items;
+    }
+    message = "";
+  }
+
+  function keyDown(e) {
+    if (e.key == "Enter") {
+      add();
+    }
+  }
+  $: doneItems = items.filter((x) => x.isDone == true);
+  $: notDoneItems = items.filter((x) => x.isDone == false);
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <input on:keydown={(e) => keyDown(e)} type="text" bind:value={message} />
+  <button on:click={add}>Add</button>
+  <h1>All items</h1>
+  <ul>
+    {#each items as item}
+      <li>{item.name} <input type="checkbox" bind:checked={item.isDone} /></li>
+    {/each}
+  </ul>
 
-  <div class="card">
-    <Counter />
-  </div>
+  <h1>Done items</h1>
+  <ul>
+    {#each doneItems as item}
+      <li>{item.name} <input type="checkbox" bind:checked={item.isDone} /></li>
+    {/each}
+  </ul>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h1>Not done items</h1>
+  <ul>
+    {#each notDoneItems as item}
+      <li>{item.name} <input type="checkbox" bind:checked={item.isDone} /></li>
+    {/each}
+  </ul>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
